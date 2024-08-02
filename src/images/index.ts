76db12cf5d73a092e772f2d8e4ed638ab2fff532
Luis Agreda <luis.agreda98@gmail.com>
2024-08-02 14:13:02 -0400

    first commit

---
+++ src/images/index.ts
@@ -0,0 +1,21 @@
+import bear from "./bear.png";
+import friends from "./friends.png";
+import coin from "./coin.png";
+import highVoltage from "./high-voltage.png";
+import notcoin from "./notcoin.png";
+import rocket from "./rocket.png";
+import trophy from "./trophy.png";
+import wallet from "./Wallet.png";
+import gentleman from "./Gentleman.png";
+
+export {
+    bear,
+    coin,
+    highVoltage,
+    notcoin,
+    gentleman,
+    rocket,
+    trophy,
+    friends,
+    wallet
+}
